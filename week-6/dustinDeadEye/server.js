8ec5138da769e0416cf1f4f88a2044bb0e85e11d
loganbunker <loganbunker@gmail.com>
2015-08-10 13:59:41 -0600

    more changes

--- week-6/dustinDeadEye/server.js
+++ week-6/dustinDeadEye/server.js
@@ -1,9 +1,18 @@
 ///////  dependencies ////////
+
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var request = require('request');
-var twilio = require('twilio');
+var twilio = require('twilio')
+
+var accountSid = '';
+var authToken = '';
+
+var client = require('twilio')(accountSid, authToken);
+
+
 
 var app = express();
 var port = 3033;
@@ -29,5 +38,13 @@ app.get('/api/message', function(req, res){
 });
 app.post('/api/send_text_message', function(req, res){
   console.log(req.body.message);
+  // request.post('https://' + accountSid + ':' + authToken + '@api.twilio.com/2010-04-01/Accounts/')
+  client.messages.create({
+	  to: "4355311845",
+	  from: "+14352384913",
+	  body: "Wasssss up?!",
+}, function(err, message) {
+	   console.log(message.sid);
+});
   res.send()
 });
